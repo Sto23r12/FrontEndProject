a8eed22db449c2e14feb473d37047806ee5e96cf
Charlie Storer <charliestorer1997@gmail.com>
2023-05-30 10:19:52 +0100

    allow cross origin resource sharing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
+const cors = require("cors");
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return <cors></cors>;
 }
 
 export default App;
