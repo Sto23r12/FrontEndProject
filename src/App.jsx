b1c11b12b893648d444db5b7008b9748ed97c92f
Charlie Storer <charliestorer1997@gmail.com>
2023-05-30 11:43:14 +0100

    Plan for ticket 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import GetHeader from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
-  return;
+  const [user, setUser] = useState("");
+  return (
+    <BrowserRouter>
+      <>
+        <GetHeader />
+      </>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function GetHeader() {
+  return (
+    <header>
+      <h1>Games Review Page!</h1>
+    </header>
+  );
+}
+
+export default GetHeader;
