397cc6c0447d2d1e900e33ef055935f30937afc6
Charlie Storer <charliestorer1997@gmail.com>
2023-06-02 08:40:59 +0100

    Updates

--- src/components/CommentSection.jsx
+++ src/components/CommentSection.jsx
@@ -1,27 +1,28 @@
 import "../App.css";
 import { fixDateFormat } from "../../utils";
 function CommentSection({ comments }) {
-  if (comments.length === 0) {
+  console.log(comments.msg === "Not found");
+  if (comments.msg === "Not found") {
     return <h2>No Comment Found!</h2>;
+  } else {
+    return (
+      <section className="CommentSection">
+        <ul>
+          {comments.comments.map((comment) => {
+            console.log(Object.keys(comment));
+            return (
+              <li key={comment.comment_id}>
+                <p>Comment: {comment.body}</p>
+                <p>By: {comment.author}</p>
+                <p>Created At: {fixDateFormat(comment.created_at)}</p>
+                <p>Votes: {comment.votes}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    );
   }
-
-  return (
-    <section className="CommentSection">
-      <ul>
-        {comments.comments.map((comment) => {
-          console.log(Object.keys(comment));
-          return (
-            <li key={comment.comment_id}>
-              <p>Comment: {comment.body}</p>
-              <p>By: {comment.author}</p>
-              <p>Created At: {fixDateFormat(comment.created_at)}</p>
-              <p>Votes: {comment.votes}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
 }
 
 export default CommentSection;
